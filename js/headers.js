998d67130ada0af9fd50c12d8f9263d02da58b6f
Andrei Iordache <51173336+andejorden@users.noreply.github.com>
2019-10-08 21:29:53 +0300

    Update headers.js

--- js/headers.js
+++ js/headers.js
@@ -1,7 +1,7 @@
-var homePage = "/index.html";
-var productPage = "/product-page.html";
-var cartPage = "/cart.html";
-var adminPage = "/admin.html";
+var homePage = "/ai-mag/index.html";
+var productPage = "/ai-mag/product-page.html";
+var cartPage = "/ai-mag/cart.html";
+var adminPage = "/ai-mag/admin.html";
 var header = `<hgroup>
     <h1 class="text-success"><a href="index.html" class="card-link"><i class="fas fa-eye"></i> <strong>AI-Mag</strong></a> - <small>My First Online Virtual Shop</small></h1>
     <h2 class="text-info">The Final Project</h2>
@@ -25,13 +25,13 @@ var bootstrapSlideShow = `<div id="demo" class="carousel slide" data-ride="carou
   <!-- The slideshow -->
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="/img/slide01.jpg" alt="Los Angeles" width="1100" height="500">
+      <img src="/ai-mag/img/slide01.jpg" alt="Los Angeles" width="1100" height="500">
     </div>
     <div class="carousel-item">
-      <img src="/img/slide02.jpg" alt="Chicago" width="1100" height="500">
+      <img src="/ai-mag/img/slide02.jpg" alt="Chicago" width="1100" height="500">
     </div>
     <div class="carousel-item">
-      <img src="/img/slide03.jpg" alt="New York" width="1100" height="500">
+      <img src="/ai-mag/img/slide03.jpg" alt="New York" width="1100" height="500">
     </div>
   </div>
   
