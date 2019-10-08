4e64f05b391b91b2637816142c572dc69e5d307e
Andrei Iordache <51173336+andejorden@users.noreply.github.com>
2019-10-08 21:23:05 +0300

    Update headers.js

--- js/headers.js
+++ js/headers.js
@@ -1,1 +1,45 @@
+var homePage = "/index.html";
+var productPage = "/product-page.html";
+var cartPage = "/cart.html";
+var adminPage = "/admin.html";
+var header = `<hgroup>
+    <h1 class="text-success"><a href="index.html" class="card-link"><i class="fas fa-eye"></i> <strong>AI-Mag</strong></a> - <small>My First Online Virtual Shop</small></h1>
+    <h2 class="text-info">The Final Project</h2>
+    <h3>HTML5, Bootstrap & Javascript</h3>
+</hgroup>`;
+var footer = `<footer class="jumbotron text-center rounded">
+    <div>
+        <p><i class="fas fa-copyright"></i> <strong>AI Copyright</strong> - All rights reserved!</p>
+    </div>
+</footer>`;
 
+var bootstrapSlideShow = `<div id="demo" class="carousel slide" data-ride="carousel">
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="/img/slide01.jpg" alt="Los Angeles" width="1100" height="500">
+    </div>
+    <div class="carousel-item">
+      <img src="/img/slide02.jpg" alt="Chicago" width="1100" height="500">
+    </div>
+    <div class="carousel-item">
+      <img src="/img/slide03.jpg" alt="New York" width="1100" height="500">
+    </div>
+  </div>
+  
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>`;
