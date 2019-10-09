b56e0e318df14bc2c8c149a537bdd577f2a650d3
Andrei Iordache <51173336+andejorden@users.noreply.github.com>
2019-10-09 15:09:00 +0300

    Update headers.js

--- js/headers.js
+++ js/headers.js
@@ -1,7 +1,7 @@
-var homePage = "/index.html";
-var productPage = "/product-page.html";
-var cartPage = "/cart.html";
-var adminPage = "/admin.html";
+var homePage = "/ai-mag/index.html";
+var productPage = "/ai-mag/product-page.html";
+var cartPage = "/ai-mag/cart.html";
+var adminPage = "/ai-mag/admin.html";
 
 var bootstrapSlideShow = `<div id="demo" class="carousel slide" data-ride="carousel">
 
@@ -15,13 +15,13 @@ var bootstrapSlideShow = `<div id="demo" class="carousel slide" data-ride="carou
   <!-- The slideshow -->
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="/img/slide01.jpg" alt="Los Angeles" width="1100" height="500">
+      <img src="/ai-mag/img/slide01.jpg" alt="Los Angeles" width="1100" height="500">
     </div>
     <div class="carousel-item">
-      <img src="/img/slide02.jpg" alt="Chicago" width="1100" height="500">
+      <img src="/ai-mag/img/slide02.jpg" alt="Chicago" width="1100" height="500">
     </div>
     <div class="carousel-item">
-      <img src="/img/slide03.jpg" alt="New York" width="1100" height="500">
+      <img src="/ai-mag/img/slide03.jpg" alt="New York" width="1100" height="500">
     </div>
   </div>
   
